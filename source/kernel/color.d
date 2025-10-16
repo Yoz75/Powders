81f60b95a74c3d116b6f34b211ca6b34290bf66e
yoz75 <levtannn@gmail.com>
2025-10-16 15:21:47 +0300

    made color serializable

--- source/kernel/color.d
+++ source/kernel/color.d
@@ -1,5 +1,7 @@
 module kernel.color;
 
+import kernel.jsonutil;
+
 /// 0xFFFFFF color
 enum Color white = Color(255, 255, 255);
 enum Color black = Color(0, 0, 0);
@@ -7,10 +9,15 @@ enum Color red = Color(255, 0, 0);
 enum Color green = Color(0, 255, 0);
 enum Color blue = Color(0, 0, 255);
 
+/// RGBA color. This struct is serializable
 public struct Color
 {
+    mixin MakeJsonizable;
 public:
-    ubyte r, g, b, a;    
+    @JsonizeField ubyte r;
+    @JsonizeField ubyte g;
+    @JsonizeField ubyte b;
+    @JsonizeField ubyte a;    
 
     this(ubyte r, ubyte g, ubyte b, ubyte a = 255)
     {
