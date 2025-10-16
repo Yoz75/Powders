3100207262446ff562c93d6a34cd0861cd6516b0
yoz75 <levtannn@gmail.com>
2025-10-16 15:11:59 +0300

    made it more beautiful

--- source/app.d
+++ source/app.d
@@ -1,6 +1,7 @@
+import core.runtime;
 import std.stdio;
 import powders.entry;
-import core.runtime;
+import colorize;
 
 extern(C) void main()
 {
@@ -15,9 +16,24 @@ void dMain()
 	{
 		powdersMain();
 	}
-	catch(Throwable ex)
+	catch(Throwable ex) // no, that's a good idea because we terminate our programm immediately
 	{
-		writeln("Unhandled error or exception: " ~ ex.msg);
+		cwriteln("Unhandled error or exception: ".color(fg.red), ex.msg.color(fg.red));
+		cwriteln("Stack trace:\n".color(fg.red));
+
+		foreach(i, info; ex.info)
+		{
+			string strInfo = cast(string) info[0..$];
+			
+			if(i % 2 == 0)
+			{
+				cwriteln(strInfo.color(fg.red));
+			}
+			else
+			{
+				cwriteln(strInfo.color(fg.yellow));
+			}
+		}
 	}
 	finally
 	{
