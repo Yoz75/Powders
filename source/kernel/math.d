c812867415383865a9e40ae38bc8f39926d49cc6
yoz75 <levtannn@gmail.com>
2025-10-28 18:55:04 +0300

    added pure modifier

--- source/kernel/math.d
+++ source/kernel/math.d
@@ -2,7 +2,7 @@ module kernel.math;
 
 import std.traits: isNumeric;
 
-public T remap(T)(T value, T fromMin, T fromMax, T toMin, T toMax) if(isNumeric!T)
+public pure T remap(T)(T value, T fromMin, T fromMax, T toMin, T toMax) if(isNumeric!T)
 {
     T t = (cast(T) (value - fromMin)) / (cast(T) (fromMax - fromMin));
     return toMin + t * (toMax - toMin);
