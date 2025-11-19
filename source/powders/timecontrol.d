ffa9192d2c881801c1297328963ad50083100319
yoz75 <levtannn@gmail.com>
2025-11-19 19:40:46 +0300

    added time steps

--- source/powders/timecontrol.d
+++ source/powders/timecontrol.d
@@ -13,11 +13,25 @@ public enum GameState
 
 public class TimeControlSystem : BaseSystem
 {
+    /// Should we process 1 frame of the game or not?
+    private bool shouldUpdate1Frame;
     public override void update()
     {
+        if(shouldUpdate1Frame && globalGameState == globalGameState.play)
+        {
+            globalGameState = GameState.pause;
+        }
+
         if(Input.isKeyPressed(Keys.space))
         {
             globalGameState = globalGameState == GameState.play ? GameState.pause : GameState.play;
+            shouldUpdate1Frame = false;
+        }
+
+        if(Input.isKeyPressed(Keys.f))
+        {
+            shouldUpdate1Frame = true;
+            globalGameState = globalGameState.play;
         }
     }
 }
