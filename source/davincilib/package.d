aac9528a532dd89ad0542b89ee979be0a385fd5f
yoz75 <levtannn@gmail.com>
2025-11-27 17:38:14 +0300

    added package.d

--- source/davincilib/package.d
+++ source/davincilib/package.d
@@ -1,3 +1,4 @@
+/// DavinciLib is a library, that provides abstractions for basic business logic: rendering the picture to user and getting input from user.
 module davincilib;
 
 public import davincilib.raylibimpl;
