25053fd564e781aeef6274f21aacacad9a705b28
yoz75 <levtannn@gmail.com>
2025-11-27 17:47:26 +0300

    added package.d

---
+++ source/kernel/package.d
@@ -0,0 +1,10 @@
+/// Kernel is a library that provides generic logic and abstractions for the game. This does not include rendering, input, or other game-specific logic.
+module kernel;
+
+public import kernel.ecs;
+public import kernel.jsonutil;
+public import kernel.math;
+public import kernel.optional;
+public import kernel.simulation;
+public import kernel.todo;
+public import kernel.versions;
