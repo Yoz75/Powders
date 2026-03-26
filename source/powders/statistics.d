e457c4946980b78888b7c7dbef2d0f1884c0be29
yoz75 <levtannn@gmail.com>
2026-03-26 16:56:31 +0300

    added fps counter

--- source/powders/statistics.d
+++ source/powders/statistics.d
@@ -21,6 +21,7 @@ public class InitialStatisticsSystem : BaseSystem
     public override void onCreated()
     {
         SystemFactory!ParticleInfoSystem.create();
+        SystemFactory!FPSInfoSystem.create();
     }
 }
 
@@ -107,5 +108,36 @@ public:
 
         immutable Particle particle = entity.getComponent!Particle();
         return particle.typeId.dup;
+    }
+}
+
+private class FPSInfoSystem : BaseSystem
+{
+    import powders.ui;
+
+    private UIText text;
+    private float timer = 0;
+    /// Draw FPS every `fpsDrawInterval` seconds
+    private enum fpsDrawInterval = 0.05;
+    
+    public override void onCreated()
+    {
+        text = new UIText();
+        text.position = [0.015, 0.03];
+    }
+
+    protected override void onUpdated()
+    {
+        import powders.rendering;
+        import std.conv : to;
+
+        immutable deltaTime = gameWindow.getDeltaTime();
+        timer += deltaTime;
+
+        if(timer >= fpsDrawInterval)
+        {
+            timer = 0;
+            text.text = "FPS: " ~ to!string(1 / gameWindow.getDeltaTime()) ~ '\0';
+        }
     }
 }
